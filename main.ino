29ccd10f305bce9992beb4fb8fab9cbfe165533b
Gabriel Neto017 <gabriel_nikel@hotmail.com>
2021-12-04 12:44:09 -0300

    Começo de Desenvolvimento  da Consulta a Lista de Cadastrados

--- main.ino
+++ main.ino
@@ -5,6 +5,8 @@ String tipo[5];
 int greenLed = 2;
 int redLed = 3;
 
+int i = 0; 
+
 void setup() {
   // Serial Configuration:
   Serial.begin(9600);
@@ -31,16 +33,24 @@ void loop() {
  
   if(selecao=="1"){
  
+      i++;
       Serial.println("Entre com o seu nome: ");
     // wait for user input through serial
       while(Serial.available() == 0);
 
       //read the input
       String id = Serial.readString();
-      id.trim();// trim to remove the return character
+
+    
+      nome[i] = id;
   }
   
   if(selecao=="2"){
+    for(int j =0 ; j<5 ; j++){
+
+        Serial.print("" +nome[j]);
+      	Serial.println("" +tipo[j]);
+      }
     
   }
   if(selecao=="3"){
